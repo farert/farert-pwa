f307767ea6ca879db8b32e6ba2cd9f6f65b9d432
Sutezo <sutezo666@gmail.com>
2026-03-15 12:43:13 +0900

    Fix Setup PWA home menu icon.

--- vite.config.ts
+++ vite.config.ts
@@ -52,12 +52,12 @@ export default defineConfig({
 				start_url: base,
 				icons: [
 					{
-						src: '/icon-192.png',
+						src: `${base}icon-192.png`,
 						sizes: '192x192',
 						type: 'image/png'
 					},
 					{
-						src: '/icon-512.png',
+						src: `${base}icon-512.png`,
 						sizes: '512x512',
 						type: 'image/png'
 					}
