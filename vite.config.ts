47b7280456b376108080a7bbde6944fff2d0f100
Sutezo <sutezo666@gmail.com>
2026-03-15 17:34:37 +0900

    fix problem home icon.

--- vite.config.ts
+++ vite.config.ts
@@ -52,12 +52,12 @@ export default defineConfig({
 				start_url: base,
 				icons: [
 					{
-						src: `${base}icon-192.png`,
+						src: 'icon-192.png',
 						sizes: '192x192',
 						type: 'image/png'
 					},
 					{
-						src: `${base}icon-512.png`,
+						src: 'icon-512.png',
 						sizes: '512x512',
 						type: 'image/png'
 					}
